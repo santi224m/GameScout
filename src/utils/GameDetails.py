683b3b9038b0ef728f7c63cfd066d12f61f78f91
CaelenO42 <caelen@olearnek.com>
2024-09-26 14:30:08 -0700

    Refactor GameDetails.py
    Changed the name of some of the variables to more closely match the Steam API, updated the reviews api for more accurate counts, and fixed a bug with hltb

--- src/utils/GameDetails.py
+++ src/utils/GameDetails.py
@@ -13,29 +13,41 @@ class GameDetails :
     self.title = None
     self.short_description = None
     self.detailed_description = None
-    self.header_img_url = None
-    self.pc_requirements_min = None
-    self.pc_requirements_recommended = None
-    self.mac_requirements_min = None
-    self.mac_requirements_recommended = None
-    self.linux_requirements_min = None
-    self.linux_requirements_recommended = None
-    self.steam_price = None
-    self.steam_discount = None
-    self.is_available_windows = False
-    self.is_available_mac = False
-    self.is_available_linux = False
+    self.header_img = None
+    self.platforms = {
+      'windows': False,
+      'mac': False,
+      'linux': False
+    }
+    self.pc_requirements = {
+      'minimum': None,
+      'recommended': None
+    }
+    self.mac_requirements = {
+      'minimum': None,
+      'recommended': None
+    }
+    self.linux_requirements = {
+      'minimum': None,
+      'recommended': None
+    }
+    self.metacritic = {
+      'score': None,
+      'url': None
+    }
+    self.categories = []
     self.screenshots = []
+    self.achievements = None
     self.release_date = None
+    self.esrb_rating = None
 
     # Steam Reviews
-    self.review_count = None
+    self.total_reviews = None
     self.positive_reviews = None
     self.negative_reviews = None
     self.steam_reviews = []
     
     # HowLongToBeat
-    # TODO: Add HLTB data here (i'll do it soon)
     self.hltb = {
       'link': None,
       'main': None,
@@ -54,7 +66,8 @@ class GameDetails :
     self.update_with_cheap_shark_api()
 
   def update_with_hltb(self):
-    results_list = HowLongToBeat().search(self.title)
+    """Update HowLongToBeat data using howlongtobeatpy"""
+    results_list = HowLongToBeat().search(self.title.lower())
     if results_list is not None and len(results_list) > 0:
         best_element = max(results_list, key=lambda element: element.similarity)
         self.hltb['link'] = best_element.game_web_link
@@ -63,6 +76,7 @@ class GameDetails :
         self.hltb['completionist'] = self.hltb_format(best_element.completionist)
         self.hltb['all_styles'] = self.hltb_format(best_element.all_styles)
     else: self.hltb = None
+    print(str(self.hltb))
 
 
   def update_with_cheap_shark_api(self):
@@ -138,32 +152,43 @@ class GameDetails :
     self.title = app_details['name']
     self.short_description = app_details['short_description']
     self.detailed_description = app_details['detailed_description']
-    self.header_img_url = app_details['header_image']
-    self.is_available_windows = app_details['platforms']['windows']
-    self.is_available_mac = app_details['platforms']['mac']
-    self.is_available_linux = app_details['platforms']['linux']
-    if self.is_available_windows:
-      if 'minimum' in app_details['pc_requirements']:
-        self.pc_requirements_min = app_details['pc_requirements']['minimum']
-      if 'recommended' in app_details['pc_requirements']:
-        self.pc_requirements_recommended = app_details['pc_requirements']['recommended']
-    if self.is_available_mac:
-      if 'minimum' in app_details['mac_requirements']:
-        self.mac_requirements_min = app_details['mac_requirements']['minimum']
-      if 'recommended' in app_details['mac_requirements']:
-        self.mac_requirements_recommended = app_details['mac_requirements']['recommended']
-    if self.is_available_linux:
-      if 'minimum' in app_details['linux_requirements']:
-        self.linux_requirements_min = app_details['linux_requirements']['minimum']
-      if 'recommended' in app_details['linux_requirements']:
-        self.linux_requirements_recommended = app_details['linux_requirements']['recommended']
-    if 'price_overview' in app_details:
-      self.steam_price = app_details['price_overview']['final_formatted']
-      self.steam_discount = app_details['price_overview']['discount_percent']
-    self.screenshots = [screenshot['path_full'] for screenshot in app_details['screenshots']]
+    self.header_img = app_details['header_image']
+    self.platforms = app_details['platforms']
+    if self.platforms['windows']:
+      self.pc_requirements = app_details['pc_requirements']
+    if self.platforms['mac']:
+      self.mac_requirements = app_details['mac_requirements']
+    if self.platforms['linux']:
+      self.linux_requirements = app_details['linux_requirements']
+    if 'metacritic' in app_details:
+      self.metacritic = app_details['metacritic']
+    self.categories = app_details['categories']
+    self.screenshots = app_details['screenshots']
+    if 'achievements' in app_details:
+       self.achievements = app_details['achievements']
     self.release_date = app_details['release_date']['date']
+    if 'ratings' in app_details and 'esrb' in app_details['ratings']:
+       self.esrb_rating = app_details['ratings']['esrb']
 
   def update_with_steam_reviews_api(self):
+    # Get total review numbers by doing all languanges
+    url_params = {
+      'appids': self.steam_app_id,
+      'json': 1,
+      'filter': 'all',
+      'language': 'all',
+      'purchase_type': 'all',
+      'review_type': 'all',
+      'num_per_page': 1
+      }
+    res = requests.get(f'https://store.steampowered.com/appreviews/{self.steam_app_id}', params=url_params)
+    app_reviews = res.json()['query_summary']
+
+    self.total_reviews = app_reviews['total_reviews']
+    self.positive_reviews = app_reviews['total_positive']
+    self.negative_reviews = app_reviews['total_negative']
+
+    # Get only english reviews for display
     url_params = {
       'appids': self.steam_app_id,
       'json': 1,
@@ -171,13 +196,11 @@ class GameDetails :
       'language': 'english',
       'purchase_type': 'all',
       'review_type': 'all',
-      'num_per_page': 5
+      'num_per_page': 10
       }
     res = requests.get(f'https://store.steampowered.com/appreviews/{self.steam_app_id}', params=url_params)
     app_reviews = res.json()
-    self.review_count = app_reviews['query_summary']['total_reviews']
-    self.positive_reviews = app_reviews['query_summary']['total_positive']
-    self.negative_reviews = app_reviews['query_summary']['total_negative']
+    
     self.steam_reviews = app_reviews['reviews']
 
   def hltb_format(self, num):
