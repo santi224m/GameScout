72b58cccd5626886929c48863eb5043a8ac026da
kendrik <kendrikdeleoz@csu.fullerton.edu>
2024-10-06 01:18:51 -0700

    not finished.

--- src/static/js/sign_up.js
+++ src/static/js/sign_up.js
@@ -1,3 +1,27 @@
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20}$/;
+
+// (           # Start of group
+//     (?=.*\d)      #   must contains one digit from 0-9
+//     (?=.*[a-z])       #   must contains one lowercase characters
+//     (?=.*[\W])        #   must contains at least one special character
+//                 .     #     match anything with previous condition checking
+//                   {8,20}  #        length at least 8 characters and maximum of 20 
+//   )           # End of group
+
+let check = false;
+let emailCheck = false;
+let passwordCheck = false;
+
+function regexCheck(password) {
+    if(passwordRegex.test(password)){
+        console.log("Password is valid");
+        return true;
+    } else {
+        console.log("Password is invalid.");
+        return false;
+    }
+}
+
 document.getElementById('continue_button').addEventListener('click', function(){
     //get the values
     const email = document.getElementById('email').value;
@@ -8,9 +32,22 @@ document.getElementById('continue_button').addEventListener('click', function(){
     console.log('Confirm Email: ', confirmEmail);
     console.log('Password: ', password);
 
+    // check emails if empty
+    
     if (email !== confirmEmail){
         alert("Emails do not match!");
+        
     }else{
         alert("matched");
+        emailCheck = true;
     }
+    if (regexCheck(password)) {
+        alert("Password is valid!")
+        passwordCheck = true;
+    } else {
+        alert("invalid password")
+    }
+    
 });
+
+//document.getElementById("check").addEventListener')
