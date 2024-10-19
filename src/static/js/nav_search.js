703e7e41d74fb3eba9f543acd4bea65b393605ea
CaelenO42 <caelen@olearnek.com>
2024-10-19 12:37:08 -0700

    Make steam default search engine

--- src/static/js/nav_search.js
+++ src/static/js/nav_search.js
@@ -6,7 +6,7 @@ function redirect_to_search_page(e) {
   // Redirect when user hits ENTER
   if (e.keyCode == 13) {
     search_term = document.querySelector('input[name="nav-search"]').value;
-    if (e.shiftKey) window.location = '/search?e=s&t=' + search_term;
-    else window.location = '/search?e=cs&t=' + search_term;
+    if (e.shiftKey) window.location = '/search?e=cs&t=' + search_term;
+    else window.location = '/search?e=s&t=' + search_term;
   }
 }
