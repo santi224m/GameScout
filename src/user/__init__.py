afba6eaa12d836fc5cc0e5db4283fd2593afcf25
kendrik <kendrikdeleoz@csu.fullerton.edu>
2024-10-20 11:03:43 -0700

    added some routes, commiting to have it tracked

--- src/user/__init__.py
+++ src/user/__init__.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 user_bp = Blueprint('user', __name__, static_folder="static", static_url_path='../static/')
 signup_bp = Blueprint('signup', __name__, static_folder="static", static_url_path='../static/')
 signin_bp = Blueprint('signin', __name__, static_folder="static", static_url_path='../static/')
-
+support_bp = Blueprint('support', __name__, static_folder="static", static_url_path='../static/')
 from src.user import routes
--- src/user/routes.py
+++ src/user/routes.py
@@ -1,7 +1,7 @@
 import requests
 
 from flask import Blueprint, render_template, request, abort
-from src.user import user_bp, signup_bp, signin_bp
+from src.user import user_bp, signup_bp, signin_bp, support_bp
 from src.utils.db_user import db_user
 
 @user_bp.route('/', methods=('GET', 'POST'))
@@ -15,3 +15,7 @@ def signup():
 @signin_bp.route('/', methods=('GET', 'POST'))
 def signin():
     return render_template('user/sign_in.html')
+
+@support_bp.route('/', methods=('GET', 'POST'))
+def support():
+    return render_template('user/support.html')
