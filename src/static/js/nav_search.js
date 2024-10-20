92e0266ab145a7db319ce65520ffbdaa137f8df0
CaelenO42 <caelen@olearnek.com>
2024-10-19 17:34:15 -0700

    Make the nav search button work

--- src/static/js/nav_search.js
+++ src/static/js/nav_search.js
@@ -2,9 +2,9 @@
 This function redirects the user when they hit ENTER on the navbar search bar.
 */
 
-function redirect_to_search_page(e) {
+function redirect_to_search_page(e, button) {
   // Redirect when user hits ENTER
-  if (e.keyCode == 13) {
+  if (e.keyCode == 13 || button) {
     search_term = document.querySelector('input[name="nav-search"]').value;
     if (e.shiftKey) window.location = '/search?e=cs&t=' + search_term;
     else window.location = '/search?e=s&t=' + search_term;
