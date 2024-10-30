460c9503cd227cd1d9518d56f30c34ffbfd0bfa7
CaelenO42 <caelen@olearnek.com>
2024-10-30 11:05:29 -0700

    Added verification email

--- src/utils/MailSender.py
+++ src/utils/MailSender.py
@@ -4,18 +4,28 @@ import os
 class MailSender:
   def __init__(self):
     self.mailer = emails.NewEmail(os.getenv("MAILERSEND_API_KEY"))
-  def send_mail(self, recipient, subject, message, message_html):
+  def send_mail(self, recipient, subject, template):
     mail_from = {"name": "GameScout", "email":"gs@trial-jpzkmgqrzkn4059v.mlsender.net"}
     recipients = [{"email": recipient}]
+    personalization = [
+      {
+        "email": recipient,
+        "data": {
+          "link": "127.0.0.1:5000/verify"
+        }
+      }
+    ]
     mail_body={}
 
     self.mailer.set_mail_from(mail_from, mail_body)
     self.mailer.set_mail_to(recipients, mail_body)
     self.mailer.set_subject(subject, mail_body)
-    self.mailer.set_html_content(message_html, mail_body)
-    self.mailer.set_plaintext_content(message, mail_body)
+    self.mailer.set_template(template, mail_body)
+    self.mailer.set_personalization(personalization, mail_body)
 
     self.mailer.send(mail_body)
 
 email = ""
-MailSender().send_mail(email, "GameScout Mail Test", "hello!", "<h1>Hello!</h1>")
+username = ""
+subject = "Hi " + username + ", please verify your GameScout account"
+MailSender().send_mail(email, subject, "3zxk54vww2xljy6v")
