94c94733e8acbfec2902191fed50634bedf7d6ca
CaelenO42 <caelen@olearnek.com>
2024-10-30 10:04:46 -0700

    Swapped Email from Mailgun to Mailersend

--- src/utils/MailSender.py
+++ src/utils/MailSender.py
@@ -1,16 +1,21 @@
-import requests
+from mailersend import emails
 import os
 
 class MailSender:
   def __init__(self):
-    pass
-  def send_mail(recipient, subject, message):
-    key = os.getenv("MAILGUN_API_KEY");
-    if key is None: raise ValueError('MAILGUN_API_KEY is not set properly in your .env file')
-    return requests.post(
-      "https://api.mailgun.net/v3/sandbox6b87086f1f834312911823811e062375.mailgun.org/messages",
-      auth=("api", key),
-      data={"from": "GameScout <mailgun@sandbox6b87086f1f834312911823811e062375.mailgun.org>",
-        "to": [recipient],
-        "subject": subject,
-        "text": message})
+    self.mailer = emails.NewEmail(os.getenv("MAILERSEND_API_KEY"))
+  def send_mail(self, recipient, subject, message, message_html):
+    mail_from = {"name": "GameScout", "email":"gs@trial-jpzkmgqrzkn4059v.mlsender.net"}
+    recipients = [{"email": recipient}]
+    mail_body={}
+
+    self.mailer.set_mail_from(mail_from, mail_body)
+    self.mailer.set_mail_to(recipients, mail_body)
+    self.mailer.set_subject(subject, mail_body)
+    self.mailer.set_html_content(message_html, mail_body)
+    self.mailer.set_plaintext_content(message, mail_body)
+
+    self.mailer.send(mail_body)
+
+email = ""
+MailSender().send_mail(email, "GameScout Mail Test", "hello!", "<h1>Hello!</h1>")
