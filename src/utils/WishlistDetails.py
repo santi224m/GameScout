093959584e8c1013ec17c9943582dd0935993a58
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-11-04 19:54:21 -0800

    Add multiprocessing to wishlist page

--- src/utils/WishlistDetails.py
+++ src/utils/WishlistDetails.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process
+
 from src.utils.GameDetails import GameDetails
 from src.utils.db_utils import db_utils
 
@@ -9,8 +11,23 @@ class WishlistDetails:
     self.num_items = 0
     self.added_date_dict = {}
 
+    # Load all games using multiprocessing first to cache them
+    p_list = []
+    for (steamid, added_date) in wishlist_data:
+      p = Process(target=self.load_game, args=(steamid,))
+      p_list.append(p)
+      p.start()
+
+    # Wait for all games to load
+    for p in p_list:
+      p.join()
+
+    # Add cached games to self.items
     for (steamid, added_date) in wishlist_data:
       self.num_items += 1
-      item = GameDetails(steamid)
+      game = GameDetails(steamid)
+      self.items.append(game)
       self.added_date_dict[steamid] = added_date.strftime("%B %d, %Y")
-      self.items.append(item)
+
+  def load_game(self, steamappid):
+    GameDetails(steamappid)
