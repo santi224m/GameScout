8e940f6ace1bc2a7fb8576f20389cc1322d70c35
CaelenO42 <caelen@olearnek.com>
2024-11-16 11:39:09 -0800

    Fix HLTB again

--- src/utils/HLTBHelper.py
+++ src/utils/HLTBHelper.py
@@ -12,16 +12,18 @@ base_url = "https://howlongtobeat.com/"
 search_url = base_url + "api/search/"
 
 class HLTB:
-  def __init__(self, title): 
+  def __init__(self, title, use_id=False): 
+    self.use_id = use_id
     self.data = self.get_game_data(title)
 
     if self.data:
+      self.game_id = self.data['game_id']
       self.main = hltb_format(self.data['comp_main'])
       self.main_extra = hltb_format(self.data['comp_plus'])
       self.all = hltb_format(self.data['comp_all'])
       self.completionist = hltb_format(self.data['comp_100'])
 
-  def get_payload(self, title, id):
+  def get_payload_id(self, title, id):
     payload = {
       "searchType": "games",
       "searchTerms": title.split(" "),
@@ -49,6 +51,62 @@ class HLTB:
       }
     }
     return json.dumps(payload)
+  
+  def get_payload_key(self, title):
+    payload = {
+      "searchType": "games",
+      "searchTerms": title.split(" "),
+      "searchPage": 1,
+      "size": 1,
+      "searchOptions": {
+        "games": {
+          "userId": 0,
+          "platform": "",
+          "sortCategory": "popular",
+          "rangeCategory": "main",
+          "rangeTime": {"min": 0, "max": 0},
+          "gameplay": {"perspective": "", "flow": "", "genre": ""},
+          "rangeYear": {"min": "", "max": ""},
+          "modifier": ""
+        },
+        "users": {"sortCategory": "postcount"},
+        "lists": {"sortCategory": "follows"},
+        "filter": "",
+        "sort": 0,
+        "randomizer": 0
+      }
+    }
+    return json.dumps(payload)
+
+  def get_key(self):
+    # Connect to Redis
+    redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+    # See if the HLTB key is already in Redis
+    if (key_cache := redis_conn.get("hltb_key")) is not None:
+      return pickle.loads(key_cache)
+    else:
+      headers = self.get_headers()
+      resp = requests.get(base_url, headers=headers)
+      if resp.status_code == 200 and resp.text is not None:
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        scripts = soup.find_all('script', src=True)
+        matching_scripts = [script['src'] for script in scripts if '_app-' in script['src']]
+        for script_url in matching_scripts:
+            script_url = base_url + script_url
+            script_resp = requests.get(script_url, headers=headers)
+            if script_resp.status_code == 200 and script_resp.text is not None:
+                pattern = r'\/api\/search\/"(?:\.concat\("[^"]*"\))*'
+                matches = re.findall(pattern, script_resp.text)
+                if matches:
+                  matches = str(matches).split('.concat')
+                  matches = [re.sub(r'["\(\)\[\]\']', '', match) for match in matches[1:]]
+                  key = ''.join(matches)
+                  key_cache = pickle.dumps(key)
+                  redis_conn.set("hltb_key", key_cache)
+                  HOUR_SECONDS = 86400
+                  redis_conn.expire("hltb_key", HOUR_SECONDS)
+                  return key
+      return None
 
   def get_id(self):
     # Connect to Redis
@@ -81,19 +139,25 @@ class HLTB:
   def get_headers(self):
     ua = UserAgent()
     headers = {
-        "User-Agent": ua.random.strip(),
+        "user-agent": ua.random.strip(),
         "referer": base_url,
-        "Accepts": "*/*",
-        "Content-Type": "application/json"
+        "accept": "*/*",
+        "content-type": "application/json"
     }
     return headers
 
   def get_game_data(self, title):
     headers = self.get_headers()
-    user_id = self.get_id()
-    payload = self.get_payload(title, user_id)
+    if self.use_id: 
+      user_id = self.get_id()
+      payload = self.get_payload_id(title, user_id)
+      res = requests.post(search_url, headers=headers, data=payload)
+    else: 
+      key = self.get_key()
+      payload = self.get_payload_key(title)
+      url = search_url + key
+      res = requests.post(url, headers=headers, data=payload)
 
-    res = requests.post(search_url, headers=headers, data=payload)
     try:
       return res.json()['data'][0]
     except IndexError:
@@ -101,6 +165,7 @@ class HLTB:
   
   def package(self):
     if self.data: return {
+      'id': self.game_id,
       'main': self.main,
       'main_extra': self.main_extra,
       'completionist': self.all,
